6205b119e387fc2764cf7278c935f8be4c52ec0c
dtolkachev <dtolkachev.04@gmail.com>
2024-11-14 23:38:16 +0300

    Basic navigation + mui navbar

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import {Game} from "./game/Game";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+    },
+    {
+        path:"/game",
+        element: <Game/>,
+    },
+    {
+        path:"/statistics",
+        element:<div>Statistics</div>
+    },
+    {
+        path:"/profile",
+        element:<div>Profile</div>,
+    }
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
